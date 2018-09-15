e91c0730c14a834908dc2b1c11b47e76ae5df5b4
Joonho Ko <joonhok@mit.edu>
2018-09-15 14:57:10 -0400

    deleted sass/scss dependency for now

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask
 from flask import render_template
 from flask_pymongo import PyMongo
-from flaskext.sass import sass
 import config
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = config.MONGO_URI
 mongo = PyMongo(app)
-sass(app, input_dir='static/scss', output_dir='static/css') # turns *.scss into *.css
 
 @app.route('/')
-def about():
-    online_users = mongo.db.users.find_one({})
+def create():
+	online_users = mongo.db.users.find_one({})
     print(online_users)
+    return render_template('create.html')
+
+@app.route('/about')
+def about():
     return render_template('about.html')
 
 if __name__ == "__main__":
