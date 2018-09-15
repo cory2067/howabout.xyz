34d40f2faeb915d811618d68a972837eb993c2f9
Joonho Ko <joonhok@mit.edu>
2018-09-15 11:51:57 -0400

    added .gitignore and connected app to template

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 if __name__ == "__main__":
         app.run(host='127.0.0.1', port=8000)
