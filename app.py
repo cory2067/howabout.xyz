5c76739b43048e4e950ac7a3ca6272c797107f8f
dillzhang <d1998z@gmail.com>
2018-09-15 15:31:59 -0400

    getcal

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import render_template, redirect, url_for
 from flask_pymongo import PyMongo
 from flask_dance.contrib.google import make_google_blueprint, google
 import config
-import os 
+import os
+import json
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
@@ -28,19 +29,30 @@ app.register_blueprint(blueprint, url_prefix="/login")
 
 @app.route('/')
 @app.route('/index')
-def hello():
+def index():
+    return 'Hello world'
+
+@app.route ('/login')
+def login():
     if not google.authorized:
         return redirect(url_for("google.login"))
     resp = google.get("/oauth2/v2/userinfo")
     print("You are {email} on Google".format(email=resp.json()["email"]))
+    return render_template('index.html')
+
+@app.route('/getCalendars')
+def getCalendars():
+    if not google.authorized:
+        return 'Not logged in'
 
     resp = google.get("/calendar/v3/users/me/calendarList")
-    for cal in resp.json()['items']:
-        print('cal {} is {}'.format(cal['id'], cal['summary']))
 
-    online_users = mongo.db.users.find_one({})
-    print(online_users)
-    return render_template('index.html')
+    json_response = {}
+    for cal in resp.json().get('items', []):
+        summary = cal['summary']
+        id = cal['id']
+        json_response[summary] = id
+    return json.dumps(json_response)
 
 if __name__ == "__main__":
         app.run(host='127.0.0.1', port=8000)
