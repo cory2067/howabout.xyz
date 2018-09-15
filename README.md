03ad724e6b92ed591c316d615ff5c3180da1e1d9
cory2067 <cor@mit.edu>
2018-09-15 11:55:13 -0400

    Add mongodb integration.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 a HackMIT 2018 project by Cory Lynch, Dillon Zhang, Joonho Ko, and Cynthia Liu
 
 ```
+sudo pip3 install -r requirements.txt
 python3 app.py
 
 ```
--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask_pymongo import PyMongo
+import config
 
 app = Flask(__name__)
-
+app.config["MONGO_URI"] = config.MONGO_URI
+mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/index')
 def hello():
+    online_users = mongo.db.users.find_one({})
+    print(online_users)
+    return 'Hello, World!'
     return render_template('index.html')
 
 if __name__ == "__main__":
