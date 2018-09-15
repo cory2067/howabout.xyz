833d5b757ef3a07251ac401fdced7f933a16e74a
Joonho Ko <joonhok@mit.edu>
2018-09-15 14:10:40 -0400

    added basic sass

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import render_template
 from flask_pymongo import PyMongo
+from flaskext.sass import sass
 import config
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = config.MONGO_URI
 mongo = PyMongo(app)
+sass(app, input_dir='static/scss', output_dir='static/css') # turns *.sass into *.css
 
 @app.route('/')
 @app.route('/index')
